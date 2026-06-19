82d0beed569ad60289ab2c45353412df589899dc
Faisal Alshahrani <107751682+Faisal-Shahrani@users.noreply.github.com>
2026-06-19 09:44:37 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -214,6 +214,7 @@ var cnames_active = {
   "aoi": "aoijs.github.io/website",
   "aom": "scarych.github.io/aom",
   "aomex": "aomex.github.io",
+  "faisalalshahrani": "friendly-flan-44c69b.netlify.app",
   "api.just": "js-just.github.io/raw-api", // noCF
   "apicluster": "ramsunvtech.github.io/apicluster", // noCF? (don´t add this in a new PR)
   "apilyertia": "apilyertia.github.io",
